317ec1173ae673c6dc60fc51ac84bddea0bc772c
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-03-11 09:24:31 +0800

    chat: rewrite dialer with cells

--- examples/js-libp2p-example-chat/src/dialer.js
+++ examples/js-libp2p-example-chat/src/dialer.js
@@ -1,43 +1,63 @@
 /* eslint-disable no-console */
+import { writeFileSync } from "fs";
+import { Debugger, Sheet, SheetProxy } from "@okcontract/cells";
 
-import { createFromJSON } from '@libp2p/peer-id-factory'
-import { multiaddr } from '@multiformats/multiaddr'
-import { createLibp2p } from './libp2p.js'
-import peerIdDialerJson from './peer-id-dialer.js'
-import peerIdListenerJson from './peer-id-listener.js'
-import { stdinToStream, streamToConsole } from './stream.js'
+import { createFromJSON } from "@libp2p/peer-id-factory";
+import { multiaddr } from "@multiformats/multiaddr";
+import { createLibp2p } from "./libp2p.js";
+import peerIdDialerJson from "./peer-id-dialer.js";
+import peerIdListenerJson from "./peer-id-listener.js";
+import { stdinToStream, streamToConsole } from "./stream.js";
 
-async function run () {
-  const [idDialer, idListener] = await Promise.all([
-    createFromJSON(peerIdDialerJson),
-    createFromJSON(peerIdListenerJson)
-  ])
+const sheet = new Sheet();
+const debug = new Debugger(sheet);
+const proxy = new SheetProxy(sheet);
 
-  // Create a new libp2p node on localhost with a randomly chosen port
-  const nodeDialer = await createLibp2p({
-    peerId: idDialer,
-    addresses: {
-      listen: ['/ip4/0.0.0.0/tcp/0']
-    }
-  })
-
-  // Output this node's address
-  console.log('Dialer ready, listening on:')
-  nodeDialer.getMultiaddrs().forEach((ma) => {
-    console.log(ma.toString())
-  })
+async function run() {
+  const idDialer = proxy.new(createFromJSON(peerIdDialerJson), "idDialer");
+  const idListener = proxy.new(
+    createFromJSON(peerIdListenerJson),
+    "idListener",
+  );
+  const nodeDialer = idDialer.map(
+    (dial) =>
+      createLibp2p({
+        peerId: dial,
+        addresses: {
+          listen: ["/ip4/0.0.0.0/tcp/0"],
+        },
+      }),
+    "nodeDialer",
+  );
+  const listeners = nodeDialer.map(
+    (dial) => dial.getMultiaddrs().map((addr) => addr.toString()),
+    "listeners",
+  );
+  listeners.subscribe((l) => console.log(`Dialer ready, listening on: ${l})`));
 
   // Dial to the remote peer (the "listener")
-  const listenerMa = multiaddr(`/ip4/127.0.0.1/tcp/10333/p2p/${idListener.toString()}`)
-  const stream = await nodeDialer.dialProtocol(listenerMa, '/chat/1.0.0')
+  const listenerMa = idListener.map(
+    (lis) => multiaddr(`/ip4/127.0.0.1/tcp/10333/p2p/${lis.toString()}`),
+    "listenerMa",
+  );
+  const stream = proxy.map(
+    [nodeDialer, listenerMa],
+    (dial, lis) => dial.dialProtocol(lis, "/chat/1.0.0"),
+    "stream",
+  );
 
-  console.log('Dialer dialed to listener on protocol: /chat/1.0.0')
-  console.log('Type a message and see what happens')
+  stream.subscribe((stream) => {
+    // console.log({ stream });
+    console.log("Dialer dialed to listener on protocol: /chat/1.0.0");
+    console.log("Type a message and see what happens");
 
-  // Send stdin to the stream
-  stdinToStream(stream)
-  // Read the stream and output to console
-  streamToConsole(stream)
+    // Send stdin to the stream
+    stdinToStream(stream);
+    // Read the stream and output to console
+    streamToConsole(stream);
+  });
+
+  writeFileSync("dialer.dot", debug.dot("dialer"));
 }
 
-run()
+run();
